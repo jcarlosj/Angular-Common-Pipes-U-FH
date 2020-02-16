71c48d0c7eb139abe8b48a0e7119ff3a5942f118
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-02-15 19:28:04 -0500

    [+] Project: Ajustes generales

--- src/app/pipes/contrasena.pipe.ts
+++ src/app/pipes/contrasena.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ContrasenaPipe implements PipeTransform {
 
-  transform( value: any, ...args: any[]): any {
+  transform( value: any, ...args: boolean[]): string {
 
       if( args[ 0 ] ) {
           let asteriskString:string = '';
